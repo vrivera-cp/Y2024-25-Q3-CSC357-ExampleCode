3a17abf634c58eae7bc862769286c8fcd50f4969
Your Name <you@example.com>
2025-05-05 10:07:49 -0700

    Actually fix binary_read.c

--- Module5/binary_read.c
+++ Module5/binary_read.c
@@ -12,13 +12,13 @@ struct person {
 
 int main(void)
 {
-    
     struct person p;
 
     FILE *file = fopen(DATA_PATH, "r");
 
     fread(&p, sizeof(struct person), 1, file);
     p.name = malloc(NAME_BUFFER_SIZE * sizeof(char));
+
     fread(p.name, sizeof(char), NAME_BUFFER_SIZE, file);
     
     fclose(file);
